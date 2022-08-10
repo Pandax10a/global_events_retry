b1f159a17e82e0624e9f0d6194cc199e0640fdd7
panda1 <blueblue0011@gmail.com>
2022-08-09 17:49:37 -0700

    stuck at 5

---
+++ week13/lifecyle_check/src/components/LifeCycleExample.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>life cycle</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'life-cycle-example'
+    }
+</script>
+
+<style scoped>
+
+</style>
